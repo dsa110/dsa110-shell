2a3974e24aa961bf8ee0ce806bd3238d383a66a5
Ubuntu <ubuntu@dsa110maas.sas.pvt>
2021-03-31 01:58:47 +0000

    revert pyutils

--- deploy_screen.bash
+++ deploy_screen.bash
@@ -3,6 +3,6 @@
 
 for corr in 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20'; do
 
-    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v2.3.1-rc10; sleep 300"
+    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v2.3.1-rc11; sleep 300"
 
 done
