4660a708751434c7b654f46b8a8de47f990f2e1a
ubuntu <ubuntu@pm.me>
2021-06-02 00:38:58 +0000

    xengine update, cnf update

--- deploy_screen.bash
+++ deploy_screen.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 
-for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20' 'corr23'; do
+for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20'; do
 
-    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v2.3.5-rc2; sleep 300"
+    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v2.4.0-rc1; sleep 300"
 
 done
