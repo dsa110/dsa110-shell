ca4abe9f9f2b47a2cdd8456c814784c9fde1e81f
ubuntu <ubuntu@pm.me>
2021-06-15 19:32:11 +0000

    second test of dsa-64

---
+++ check_versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20' 'corr23'; do
+    echo "Status of $corr:"
+    ssh -q -o "StrictHostKeyChecking no" $corr.sas.pvt '( pip list | grep dsa110-T2 ; pip list | grep dsa110-antpos ; pip list | grep pyuvdata ; pip list | grep dsa110-pyutils ; pip list | grep dsa110-calib ; pip list | grep dsa110-meridian-fs )'
+    echo " "
+done
+
+    
