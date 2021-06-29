2538bf618b495238e4b0b538da0022fef158a61d
ubuntu <ubuntu@pm.me>
2021-06-29 09:02:34 +0000

    test thread capture

--- deploy_screen.bash
+++ deploy_screen.bash
@@ -5,6 +5,6 @@
 
 for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20'; do
 
-    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc28; sleep 300"
+    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc29; sleep 300"
 
 done
