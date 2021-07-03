f4d9445dcf0a287d0ecbda2756a14e7f21bd6d19
ubuntu <ubuntu@pm.me>
2021-07-03 00:39:47 +0000

    try

--- deploy_screen.bash
+++ deploy_screen.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-1;95;0c
+
 # v2.4.0-rc8 is the good dsa-25 one
 
 for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20'; do
 
-    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc42; sleep 300"
+    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc43; sleep 300"
 
 done
