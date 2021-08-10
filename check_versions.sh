328bdbcb7aa7ecf01b3d287e8b655d9f2a512ac3
ubuntu <ubuntu@pm.me>
2021-08-10 05:28:19 +0000

    voltage service bug

--- check_versions.sh
+++ check_versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20' 'corr23'; do
+for corr in 'corr00' 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20' 'corr21' 'corr22' 'corr23'; do
     echo "Status of $corr:"
     ssh -q -o "StrictHostKeyChecking no" $corr.sas.pvt '( pip list | grep dsa110-T2 ; pip list | grep dsa110-antpos ; pip list | grep pyuvdata ; pip list | grep dsa110-pyutils ; pip list | grep dsa110-calib ; pip list | grep dsa110-meridian-fs )'
     echo " "
