8540e803177f18b885a01da5fc33bb4c6da52aec
ubuntu <ubuntu@pm.me>
2022-05-10 15:11:28 +0000

    bug

--- deploy_screen.bash
+++ deploy_screen.bash
@@ -4,6 +4,6 @@
 
 for corr in 'corr01' 'corr02' 'corr03' 'corr04' 'corr05' 'corr06' 'corr07' 'corr08' 'corr09' 'corr10' 'corr11' 'corr12' 'corr13' 'corr14' 'corr15' 'corr16' 'corr17' 'corr18' 'corr19' 'corr20' 'corr21' 'corr22'; do
 
-    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc154; sleep 300"
+    screen -S ${corr} -dm bash -c "/home/ubuntu/proj/dsa110-shell/deploy install ${corr}.sas.pvt v3.0.0-rc155; sleep 300"
 
 done
